831098c44b62af739ae09d60ae5e1e570355153e
xavierclaeys <xavier.claeys@upmc.fr>
2021-10-25 17:48:03 +0200

    Ajout makefile et routine de test

---
+++ makefile
@@ -0,0 +1,9 @@
+
+test: test.o
+	g++ test.o -o test
+
+test.o:
+	g++ -c test.cpp -o test.o
+
+check:
+	./test
---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "smallvect.hpp"
+
+
+using namespace std;
+
+int main(){
+  
+  cout << "Hello world" << endl;  
+  R3 x;
+  cout << x << endl;
+
+  return 0;
+}
